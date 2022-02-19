164074d43061a5ccba1383843dc6661f9c479627
pragmaticsystematic <55458414+pragmaticsystematic@users.noreply.github.com>
2022-02-19 10:39:20 +0200

    initial commit

---
+++ WordGameServer/GameLogic/GameLogic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordGameServer.GameLogic
+{
+    /// <summary>
+    /// This class houses the game logic operations of the server.
+    /// It can choose a word to guess, check if a certain word exist in a dictionary and evaluate a guess
+    /// </summary>
+    public class GameLogic
+    {
+        private List<string>               _acceptableWordPool;
+        private Dictionary<string, string> _wordsToGuess;
+
+        private const string WORD_POOL_FILE_PATH =
+            @"C:\Users\wolverine1984\RiderProjects\WordGameServer\WordGameServer\Resources\all_words\words_of_length_5.csv";
+        
+        private const string COMMON_WORDS_FILE_PATH =
+            @"C:\Users\wolverine1984\RiderProjects\WordGameServer\WordGameServer\Resources\common_words\common_words_of_length_5.csv";
+
+        public GameLogic()
+        {
+            _acceptableWordPool = new List<string>();
+            _wordsToGuess       = new Dictionary<string, string>();
+            LoadAcceptableWordPoolFromCsv();
+            Console.WriteLine(_acceptableWordPool);
+        }
+
+
+        /// <summary>
+        /// Checks if a specific word exist in the acceptable word pool.
+        /// </summary>
+        /// <param name="wordToCheck">The word to check against the acceptable words pool</param>
+        /// <returns>True if this word exist in the word pool, False otherwise.</returns>
+        public bool CheckIsViableWord(string wordToCheck)
+        {
+            return _acceptableWordPool.Contains(wordToCheck.ToLower());
+        }
+
+        /// <summary>
+        /// Load the word pool that we consider to be acceptable from the WORD_POOL_FILE_PATH path.
+        /// </summary>
+        private void LoadAcceptableWordPoolFromCsv()
+        {
+            _acceptableWordPool = System.IO.File.ReadAllText(WORD_POOL_FILE_PATH).Split(',')
+                                        .Select(x => x.ToLower()).ToList();
+        }
+    }
+}
---
+++ WordGameServer/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WordGameServer
+{
+    internal class Program
+    {
+        public static int Main(string[] args)
+        {
+            // StartServer();
+            // StartTCPListener();
+            var gl = new GameLogic.GameLogic();
+            
+            return 0;
+        }
+
+        public static void StartServer()
+        {
+            // Get Host IP Address that is used to establish a connection
+            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+            // If a host has multiple addresses, you will get a list of addresses
+            IPHostEntry host          = Dns.GetHostEntry("localhost");
+            IPAddress   ipAddress     = host.AddressList[0];
+            IPEndPoint  localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            try
+            {
+                // Create a Socket that will use Tcp protocol
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                // A Socket must be associated with an endpoint using the Bind method
+                listener.Bind(localEndPoint);
+                // Specify how many requests a Socket can listen before it gives Server busy response.
+                // We will listen 10 requests at a time
+                listener.Listen(10);
+
+                Console.WriteLine("Waiting for a connection...");
+                Socket handler = listener.Accept();
+
+                // Incoming data from the client.
+                string data  = null;
+                byte[] bytes = null;
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Text received : {0}", data);
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                handler.Send(msg);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static void StartTCPListener()
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32     port      = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data  = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data =
+                            $"Hello from server! Got it! The user guess is: {data.Replace("Hello from Unity! Player guess is ", "")}";
+
+                        byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
